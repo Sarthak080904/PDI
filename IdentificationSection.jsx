b1bd08fa61a32689addbcb963ee62d4436cc6f66
Sarthak Bhujbal <159437497+Sarthak080904@users.noreply.github.com>
2025-07-02 19:07:51 +0530

    IdentificationSection.jsx

--- IdentificationSection.jsx
+++ IdentificationSection.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { v4 as uuidv4 } from 'uuid';
-import { useReportContext } from '../contexts/ReportContext';
+import { useReportContext } from 'ReportContext.jsx';
 import { FaTrash, FaPlus } from 'react-icons/fa';
 
 const IdentificationSection = () => {
